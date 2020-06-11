8269bdda8950e766becd60968414e76de112d390
Rahit <rahitkar@users.noreply.github.com>
2020-06-11 12:38:02 +0530

    modified test descriptions

--- test/testGraph.js
+++ test/testGraph.js
@@ -40,22 +40,22 @@ describe('BFS', () => {
   });
 
   describe('bfs', () => {
-    it('should validate for path Kolkata to Chennai for given pairs', () => {
+    it('should validate for path Kolkata to Chennai for given densely connected pairs', () => {
       assert.strictEqual(bfs(pairs, 'Kolkata', 'Chennai'), true);
     });
-    it('should validate for path Kolkata to pune for given pairs', () => {
+    it('should invalidate for path Kolkata to pune for given densely connected pairs', () => {
       assert.strictEqual(bfs(pairs, 'Kolkata', 'pune'), false);
     });
 
-    it('should invalidate for path Kolkata to Kolkata for given pairs', () => {
+    it('should invalidate for path Kolkata to Kolkata for given pairs where Kolkata is not connected to itself ', () => {
       assert.strictEqual(bfs([['Kolkata', 'Bangalore'], ['Bangalore', 'Chennai'], ['Chennai', 'Bangalore']], 'Kolkata', 'Kolkata'), false);
     });
 
-    it('should validate for path Kolkata to Kolkata for given paries', () => {
+    it('should validate for path Kolkata to Kolkata for given paries where Kolkata is connected to itself', () => {
       assert.strictEqual(bfs([['Kolkata', 'Bangalore'], ['Kolkata', 'Kolkata']], 'Kolkata', 'Kolkata'), true);
     });
 
-    it('should invalidate for path Kolkata to Kolkata for given pairs', () => {
+    it('should invalidate for path Kolkata to Kolkata for given pairs where Kolkata is not connected to itself and one child has no edge', () => {
       assert.strictEqual(bfs([['Kolkata', 'Bangalore'], ['Bangalore', 'Chennai']], 'Kolkata', 'Kolkata'), false);
     });
   });
